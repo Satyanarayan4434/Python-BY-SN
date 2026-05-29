2369494abd1d64d2441bcc3ce7c3d84443a3878a
Satyanarayan Sen <satyanarayan74sen@gmail.com>
2026-05-29 10:27:45 +0530

    Day 12 Python Practice Done.

---
+++ python-language-practice/python-practice-3.py
@@ -0,0 +1,14 @@
+data ={
+    'harsh' : ['Java', 'Python', 'AI'],
+    'navin' : {'frotend':'react', 'backend':'spring'},
+    'kiran' : ('js', 'flask')
+}
+
+print(data['navin']['backend'])
+print(type(data['navin']))
+
+print(data['harsh'][1])
+print(print(type(data['harsh'])))
+
+print(data['kiran'][0])
+print(type(data['kiran']))
