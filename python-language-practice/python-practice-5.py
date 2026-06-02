345c1891d018e7478dd5e245c50f0541ec0ac72b
Satyanarayan Sen <satyanarayan74sen@gmail.com>
2026-06-02 13:31:56 +0530

    Day 16 Note and Code Added

---
+++ python-language-practice/python-practice-5.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    a = 6
+    b = 7
+    c = a + b
+    print(c)
+    
+add(3, 2)
+
+# Output: 13, because the values of a and b are changed inside the function, and the original values passed as arguments (3 and 2) are not used in the calculation.
