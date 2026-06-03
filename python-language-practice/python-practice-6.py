ea5e30fe4385d914ac0df597e1b2b07c2eaa5869
Satyanarayan Sen <satyanarayan74sen@gmail.com>
2026-06-03 07:01:18 +0530

    Day - 17 Code Added

---
+++ python-language-practice/python-practice-6.py
@@ -0,0 +1,13 @@
+import math
+
+x = -10.7
+y =3
+
+print(math.ceil(x))  # Output: -10
+# The ceil function rounds a number up to the nearest integer. In this case, -10.7 is rounded up to -10.
+
+print(math.fabs(x))  # Output: 10.7
+# The fabs function returns the absolute value of a number. In this case, the absolute value of -10.7 is 10.7.
+
+print(math.pow(y, 2))  # Output: 9.0
+# The pow function returns the value of x raised to the power of y. In this case, 3 raised to the power of 2 is 9.0.
